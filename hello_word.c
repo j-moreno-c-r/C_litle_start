47b34d0a18bbc52a4279cc7477ae400657a22239
j-moreno-c-r <joaomorenocruzrosa@gmail.com>
2024-07-07 20:24:26 -0300

    hello word

---
+++ hello_word.c
@@ -0,0 +1,13 @@
+#include<stdio.h> //standard input-output
+ 
+int main() {
+    printf("Olá mundo!!!");
+    return 0;
+} 
+
+
+//organization of a function type of data : name_of_the_function: arguments:{body of function}
+ 
+    // 0 - false : everithing its ok
+    // -1 : True : error occurred
+
